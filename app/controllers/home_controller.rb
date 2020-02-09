1e031eb7c9d564d0143f48568750d55cc1330ca4
Riccardo <rik86>
2020-02-09 15:13:08 +0000

    configure fronted kit with rails including assets and fonts

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+    # GET /
+    def index
+        render 'index'
+    end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.browserify_rails.commandline_options = '-t coffeeify --extension=".coffee"'
+    config.public_file_server.enabled = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
